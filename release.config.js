d4f56a8a748ff25a67d3eb031b69f7662cd8b3d6
bajrayejoon <binaya.bajrashakya@gmail.com>
2025-02-07 15:56:05 +0545

    feat: ✨  Add automatic semantic versioning (test)

--- release.config.js
+++ release.config.js
@@ -2,12 +2,15 @@ module.exports = {
   branches: ['master'], // Adjust branch if needed
   plugins: [
     '@semantic-release/commit-analyzer', // Analyze commits to determine version bump
+    '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
+    '@semantic-release/github',
     [
       '@semantic-release/git',
       {
         assets: ['package.json', 'package-lock.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]',
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
