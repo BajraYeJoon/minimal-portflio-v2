764d6f1c6d0f8740442b8857aa496a4d8ea94551
bajrayejoon <binaya.bajrashakya@gmail.com>
2025-03-04 11:10:02 +0545

    fix: 🐛  Expose client data to env for build

--- app/sanity/client.ts
+++ app/sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  projectId: 'z68c4dv3',
   dataset: import.meta.env.VITE_SANITY_DATASET,
-  apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
+  apiVersion: '2024-01-01',
   useCdn: true,
 });
